c40fef8e67bd1e0d9224498d61342924656f18df
Ian Reah <ian.reah@gmail.com>
2013-03-17 13:29:47 +0000

    First

---
+++ Molecules3D/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Molecules3D
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+                name: "API Search",
+                routeTemplate: "api/search/{searchTerm}",
+                defaults: new { controller = "Search" });
+        }
+    }
+}
---
+++ Molecules3D/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Molecules3D
+{
+    public class SearchController : ApiController
+    {
+        public string Get(string searchTerm)
+        {
+            return string.Format("3D Chemical Structure for {0}", searchTerm);
+        }
+    }
+}
