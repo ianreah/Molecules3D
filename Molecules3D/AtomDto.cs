3e7b67ee89de73f29cf3216a6bab1805176e8548
Ian Reah <ian.reah@gmail.com>
2013-03-18 17:21:48 +0000

    Return hard-coded atoms from the search api

---
+++ Molecules3D/AtomDto.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+
+namespace Molecules3D
+{
+	[DataContract]
+	public class AtomDto
+	{
+		public AtomDto(double x, double y, double z, string element)
+		{
+			this.X = x;
+			this.Y = y;
+			this.Z = z;
+			this.Element = element;
+		}
+
+		[DataMember]
+		public double X { get; set; }
+
+		[DataMember]
+		public double Y { get; set; }
+
+		[DataMember]
+		public double Z { get; set; }
+
+		[DataMember]
+		public string Element { get; set; }
+	}
+}
--- Molecules3D/SearchController.cs
+++ Molecules3D/SearchController.cs
@@ -1,12 +1,26 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 
 namespace Molecules3D
 {
     public class SearchController : ApiController
     {
-        public string Get(string searchTerm)
+        public IEnumerable<AtomDto> Get(string searchTerm)
         {
-            return string.Format("3D Chemical Structure for {0}", searchTerm);
+			yield return new AtomDto(1.0454, 0.0621, -0.0638, "C");
+			yield return new AtomDto(2.4799, 0.0621, -0.0638, "N");
+			yield return new AtomDto(3.2950, 1.2012, -0.0625, "C");
+			yield return new AtomDto(4.5871, 0.8365, -0.0619, "N");
+			yield return new AtomDto(4.6415, -0.5434, -0.0629, "C");
+			yield return new AtomDto(3.2465, -1.0092, -0.0645, "C");
+			yield return new AtomDto(3.0089, -2.4528, -0.0656, "C");
+			yield return new AtomDto(1.8708, -2.9127, -0.0720, "O");
+			yield return new AtomDto(4.1189, -3.2786, -0.0591, "N");
+			yield return new AtomDto(5.3992, -2.7356, -0.0645, "C");
+			yield return new AtomDto(6.3574, -3.5029, -0.0707, "O");
+			yield return new AtomDto(5.6340, -1.4497, -0.0634, "N");
+			yield return new AtomDto(6.9934, -0.9915, -0.0628, "C");
+			yield return new AtomDto(3.9482, -4.7028, -0.0467, "C");
         }
     }
 }
