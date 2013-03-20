03c77e065f42f035175ba23387a392d742d4f50f
Ian Reah <ian.reah@gmail.com>
2013-03-20 13:37:19 +0000

    Added the bonds. Also, trackball controls and rotation

--- Molecules3D/scripts/main.js
+++ Molecules3D/scripts/main.js
@@ -1,21 +1,25 @@
 ﻿$(function () {
 	var width = window.innerWidth;
 	var height = window.innerHeight;
-	
+
 	var camera = new THREE.PerspectiveCamera(70, width / height, 1, 500);
 	camera.position.z = 15;
 
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(width, height);
 	$('#container').append(renderer.domElement);
-	
+
 	var ambientLight = new THREE.AmbientLight(0x555555);
 	var directionalLight = new THREE.DirectionalLight(0xffffff);
 	directionalLight.position.set(0.3, 0.5, 2);
+	camera.add(directionalLight);
+
+	var molecule = new THREE.Object3D();
 
 	var scene = new THREE.Scene();
 	scene.add(ambientLight);
-	scene.add(directionalLight);
+	scene.add(molecule);
+	scene.add(camera);
 
 	$.getJSON('api/search/anything', function (data) {
 		var atoms = data.Atoms;
@@ -28,8 +32,31 @@
 			sphere.position.x = atom.X;
 			sphere.position.y = atom.Y;
 			sphere.position.z = atom.Z;
-			scene.add(sphere);
+			molecule.add(sphere);
 		}
+
+		var bonds = data.Bonds;
+		for (var j = 0; j < bonds.length; j++) {
+			var bond = bonds[j];
+			var tube = new THREE.Mesh(
+				new THREE.TubeGeometry(new THREE.SplineCurve3([new THREE.Vector3(bond.FromX, bond.FromY, bond.FromZ), new THREE.Vector3(bond.ToX, bond.ToY, bond.ToZ)]), 64, 0.03),
+				new THREE.MeshPhongMaterial({ color: 0xF9F9F9, ambient: 0xF9F9F9 })
+			);
+			molecule.add(tube);
+		}
+	});
+
+	var controls = new THREE.TrackballControls(camera, renderer.domElement);
+	
+	jsFrames.registerAnimation(function () {
+		controls.update();
+
+		var time = Date.now() * 0.0004;
+
+		molecule.rotation.x = time;
+		molecule.rotation.y = time * 0.7;
+
 		renderer.render(scene, camera);
 	});
+	jsFrames.start();
 });
