6acf1f1ae781facb3c83bd2fcdd65f905a8f5732
Ian Reah <ian.reah@gmail.com>
2013-03-24 19:24:12 +0000

    Added width & height vars

--- Molecules3D/scripts/AR_mediastream.js
+++ Molecules3D/scripts/AR_mediastream.js
@@ -7,6 +7,9 @@
     }
 
     if (hasGetUserMedia()) {
+        var width = 320;
+        var height = 240;
+
         var inputStream = $('#inputStream')[0];
         navigator.getUserMedia({ 'video': true }, function (stream) {
             inputStream.src = window.URL.createObjectURL(stream);
@@ -21,7 +24,7 @@
         // The parameters are used to set the detector's camera parameters
         // (...will need to copy the threejs camera details into this but
         // for now I'll just start with the width & height!)
-        var parameters = new FLARParam(320, 240);
+        var parameters = new FLARParam(width, height);
 
         // The marker detector
         // (The 2nd parameter could be marker width? May need to investigate this!)
@@ -34,7 +37,7 @@
         jsFrames.registerAnimation(function () {
             // Capture the current frame from the inputStream
             // (then we need to tell the image reader that the input has changed.)
-            inputCapture.getContext('2d').drawImage(inputStream, 0, 0, 320, 240);
+            inputCapture.getContext('2d').drawImage(inputStream, 0, 0, width, height);
             inputCapture.changed = true;
 
             // Use the imageReader to detect the markers
