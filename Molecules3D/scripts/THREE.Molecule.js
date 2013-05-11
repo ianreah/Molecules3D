5a82f7e628296a5192dc3edba40f1e1d0c95029e
Ian Reah <ian.reah@gmail.com>
2013-05-11 14:39:24 +0100

    Introduce a THREE.Molecule object

---
+++ Molecules3D/scripts/THREE.Molecule.js
@@ -0,0 +1,36 @@
+﻿THREE.Molecule = function () {
+};
+
+THREE.Molecule.prototype = new THREE.Object3D();
+
+THREE.Molecule.prototype.loadAsync = function (jsonUrl, loadedCallback) {
+	// TODO: Reset the object first
+	var me = this;
+
+	$.getJSON(jsonUrl, function (data) {
+		var atoms = data.Atoms;
+		for (var i = 0; i < atoms.length; i++) {
+			var atom = atoms[i];
+			var sphere = new THREE.Mesh(
+				new THREE.SphereGeometry(0.3, 16, 16),
+				new THREE.MeshPhongMaterial({ color: atom.Color, ambient: atom.Color, shininess: 60 })
+			);
+			sphere.position.x = atom.X;
+			sphere.position.y = atom.Y;
+			sphere.position.z = atom.Z;
+			me.add(sphere);
+		}
+
+		var bonds = data.Bonds;
+		for (var j = 0; j < bonds.length; j++) {
+			var bond = bonds[j];
+			var tube = new THREE.Mesh(
+				new THREE.TubeGeometry(new THREE.SplineCurve3([new THREE.Vector3(bond.FromX, bond.FromY, bond.FromZ), new THREE.Vector3(bond.ToX, bond.ToY, bond.ToZ)]), 64, 0.03),
+				new THREE.MeshPhongMaterial({ color: 0xF9F9F9, ambient: 0xF9F9F9 })
+			);
+			me.add(tube);
+		}
+
+		loadedCallback();
+	});
+};
--- Molecules3D/scripts/main.js
+++ Molecules3D/scripts/main.js
@@ -11,38 +11,15 @@
 	directionalLight.position.set(0.3, 0.5, 2);
 	camera.add(directionalLight);
 
-	var molecule = new THREE.Object3D();
+	var molecule = new THREE.Molecule();
 
 	var scene = new THREE.Scene();
 	scene.add(ambientLight);
 	scene.add(molecule);
 	scene.add(camera);
 
-	$.getJSON('/MolFiles/caffeine.json', function (data) {
-		var atoms = data.Atoms;
-		for (var i = 0; i < atoms.length; i++) {
-			var atom = atoms[i];
-			var sphere = new THREE.Mesh(
-				new THREE.SphereGeometry(0.3, 16, 16),
-				new THREE.MeshPhongMaterial({ color: atom.Color, ambient: atom.Color, shininess: 60 })
-			);
-			sphere.position.x = atom.X;
-			sphere.position.y = atom.Y;
-			sphere.position.z = atom.Z;
-			molecule.add(sphere);
-		}
-
-		var bonds = data.Bonds;
-		for (var j = 0; j < bonds.length; j++) {
-			var bond = bonds[j];
-			var tube = new THREE.Mesh(
-				new THREE.TubeGeometry(new THREE.SplineCurve3([new THREE.Vector3(bond.FromX, bond.FromY, bond.FromZ), new THREE.Vector3(bond.ToX, bond.ToY, bond.ToZ)]), 64, 0.03),
-				new THREE.MeshPhongMaterial({ color: 0xF9F9F9, ambient: 0xF9F9F9 })
-			);
-			molecule.add(tube);
-		}
-
-		$('#loading').hide();
+	molecule.loadAsync('/MolFiles/caffeine.json', function () {
+	    $('#loading').hide();
 	});
 
 	var controls = new THREE.TrackballControls(camera, renderer.domElement);
