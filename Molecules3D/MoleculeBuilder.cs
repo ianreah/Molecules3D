6de03a9729848e95048a5d6539f65ad884d02e30
Ian Reah <ian.reah@gmail.com>
2014-11-09 19:08:22 +0000

    Added MoleculeBuilder
    MoleculeBuilder is an expression builder for building up a
    molecule from a SMILES string, generating 3D co-ordinates and
    converting to a dto object.
    
    Internally, it uses OBDotNet classes.

---
+++ Molecules3D/MoleculeBuilder.cs
@@ -0,0 +1,42 @@
+﻿using OpenBabel;
+using System.Linq;
+
+namespace Molecules3D
+{
+    public class MoleculeBuilder
+    {
+        private readonly OBMol molecule = new OBMol();
+
+        public MoleculeBuilder FromSmiles(string smiles)
+        {
+            var obconv = new OBConversion();
+            obconv.SetInFormat("smi");
+            obconv.ReadString(molecule, smiles);
+            return this;
+        }
+
+        public MoleculeBuilder Gen3D()
+        {
+            var generator = OBOp.FindType("Gen3D");
+            generator.Do(molecule);
+            return this;
+        }
+
+        public MoleculeDto ToDto()
+        {
+            var result = new MoleculeDto
+            {
+                Atoms = molecule.Atoms().Select(x => new AtomDto(x.GetX(), x.GetY(), x.GetZ(), Atoms.Colors[x.GetElementSymbol()])).CentralizeAtoms()
+            };
+
+            result.Bonds = molecule.Bonds().Select(x =>
+                {
+                    var from = result.Atoms.ElementAt((int)x.GetBeginAtomIdx()-1);
+                    var to = result.Atoms.ElementAt((int)x.GetEndAtomIdx()-1);
+
+                    return new BondDto(from.X, from.Y, from.Z, to.X, to.Y, to.Z);
+                });
+            return result;
+        }
+    }
+}
---
+++ Molecules3D/OBDotNetExtensions.cs
@@ -0,0 +1,14 @@
+﻿using OpenBabel;
+
+namespace Molecules3D
+{
+	public static class OBDotNetExtensions
+	{
+		private static readonly OBElementTable obElementTable = new OBElementTable();
+
+		public static string GetElementSymbol(this OBAtom obAtom)
+		{
+			return obElementTable.GetSymbol((int)obAtom.GetAtomicNum());
+		}
+	}
+}
